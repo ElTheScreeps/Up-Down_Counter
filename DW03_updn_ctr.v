5d6150dc9a594b332702838cd9b693e785888dbc
Gheorghe Lucian <115155585+ElTheScreeps@users.noreply.github.com>
2022-10-06 14:40:16 +0300

    Add files via upload

---
+++ DW03_updn_ctr.v
@@ -0,0 +1,40 @@
+//---------------------------------------------------------------------------------------
+// Universitatea Transilvania din Brasov
+// Proiect     : Limbaje de descriere hardware
+// Autor       : <Gheorghe Lucian Giani Andrei>
+// Data        : <10/04/2022>
+//---------------------------------------------------------------------------------------
+// Descriere   : <Up/Down Counter>
+//---------------------------------------------------------------------------------------
+
+module DW03_updn_ctr( data, up_dn, load, cen, clk, reset, count, tercnt );
+
+parameter width = 8;
+
+input  [2:0] data;
+input 		 up_dn;
+input 		 load;
+input 		 cen;
+input 		 clk;
+input 		 reset;
+output [2:0] count;
+output 		 tercnt;
+
+reg    [2:0] count_intern;
+
+always @(posedge(clk) or negedge(reset))
+begin
+	if (~reset) count_intern <= 'b0;
+	else if (cen == 0) count_intern <= count_intern;
+	else if (cen == 1)
+	begin
+		if (up_dn) count_intern <= count_intern +1;
+		else count_intern <= count_intern - 1;
+	end
+end
+
+assign count = count_intern;
+
+assign tercnt = (count_intern == 3'b111) ? 1 : 0;
+
+endmodule
---
+++ top.v
@@ -0,0 +1,47 @@
+//---------------------------------------------------------------------------------------
+// Universitatea Transilvania din Brasov
+// Proiect     : Limbaje de descriere hardware
+// Autor       : <Gheorghe Lucian Giani Andrei>
+// Data        : <10/04/2022>
+//---------------------------------------------------------------------------------------
+// Descriere   : <Testbench>
+//---------------------------------------------------------------------------------------
+module top();
+
+reg        clk;
+reg        reset;
+reg        load;
+reg        cen;
+reg 	   up_dn;
+reg  [2:0] data;
+
+wire 	   tercnt;
+wire [2:0] count;
+
+always #5 clk = ~clk; 
+
+initial begin
+clk   = 0;
+reset = 1;
+load  = 1;
+cen   = 0;
+up_dn = 1;
+data  = 0;
+repeat(5) @(posedge clk);
+reset = 0;
+repeat(5) @(posedge clk);
+reset = 1;
+load  = 0;
+repeat(15) @(posedge clk);
+load  = 1;
+repeat(10) @(posedge clk);
+cen   = 1;
+repeat(30) @(posedge clk);
+up_dn = 0;
+repeat(10) @(posedge clk);
+$stop;
+end
+
+DW03_updn_ctr U1 (.data(data), .up_dn(up_dn), .load(load), .cen(cen), .clk(clk), .reset(reset), .count(count), .tercnt(tercnt));
+
+endmodule
